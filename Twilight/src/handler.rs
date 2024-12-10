bedc3d2e2dcd262c92567181a745cc5a185a7f65
Miezhiko <Miezhiko@gmail.com>
2024-12-10 14:57:10 +0400

    minor update
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- Twilight/src/handler.rs
+++ Twilight/src/handler.rs
@@ -25,7 +25,10 @@ use regex::Regex;
 
 use twilight_gateway::Event;
 
-use twilight_model::channel::Message;
+use twilight_model::{
+  channel::Message,
+  gateway::payload::incoming::MessageCreate
+};
 
 const ME: u64 = 510368731378089984;
 
@@ -55,8 +58,46 @@ async fn help(msg: Message, state: State) -> anyhow::Result<()> {
 fn contains_bug(text: &str) -> Option<i32> {
   static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i)bug (\d+)").unwrap());
   let cap = RE.captures(text)?;
-  let number = cap[1].parse::<i32>().ok()?;
-  Some(number)
+  if cap.len() > 1 {
+    let number = cap[1].parse::<i32>().ok()?;
+    Some(number)
+  } else {
+    None
+  }
+}
+
+async fn handle_message(
+  msg: Box<MessageCreate>,
+  state: &State
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+  if msg.author.bot {
+    return Ok(());
+  }
+
+  match msg.content.split_whitespace().next() {
+    Some("-help")     => spawn(help(msg.0, Arc::clone(state))),
+    Some("-bug")      => spawn(bug(msg.0, None, Arc::clone(state))),
+    Some("-wiki")     => spawn(wiki(msg.0, Arc::clone(state))),
+    Some("-overlays") => spawn(overlays(msg.0, Arc::clone(state))),
+    Some("-register") => spawn(register(msg.0, Arc::clone(state))),
+    Some("-show")     => spawn(show(msg.0, Arc::clone(state))),
+    Some("-list")     => spawn(list(msg.0, Arc::clone(state))),
+    Some("-delete")   => spawn(delete(msg.0, Arc::clone(state))),
+    Some(cmd)         => {
+      if msg.author.id.get() == ME {
+        match cmd {
+          "-import"  => spawn(export::import(msg.0, Arc::clone(state))),
+          "-export"  => spawn(export::export(msg.0, Arc::clone(state))),
+          _category  => {}
+        }
+      } else if let Some(bug_number) = contains_bug(msg.content.as_str()) {
+        spawn(bug(msg.0, Some(bug_number), Arc::clone(state)))
+      }
+    },
+    None              => {}
+  };
+
+  Ok(())
 }
 
 pub async fn handle_event(
@@ -64,40 +105,11 @@ pub async fn handle_event(
   state: State,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
   match event {
-    Event::MessageCreate(msg) => {
-      if msg.author.bot {
-        return Ok(());
-      }
-      if msg.guild_id.is_some() || msg.content.starts_with('-') {
-        match msg.content.split_whitespace().next() {
-          Some("-help")     => spawn(help(msg.0, Arc::clone(&state))),
-          Some("-bug")      => spawn(bug(msg.0, None, Arc::clone(&state))),
-          Some("-wiki")     => spawn(wiki(msg.0, Arc::clone(&state))),
-          Some("-overlays") => spawn(overlays(msg.0, Arc::clone(&state))),
-          Some("-register") => spawn(register(msg.0, Arc::clone(&state))),
-          Some("-show")     => spawn(show(msg.0, Arc::clone(&state))),
-          Some("-list")     => spawn(list(msg.0, Arc::clone(&state))),
-          Some("-delete")   => spawn(delete(msg.0, Arc::clone(&state))),
-          Some(cmd)         => {
-            if msg.author.id.get() == ME {
-              match cmd {
-                "-import"  => spawn(export::import(msg.0, Arc::clone(&state))),
-                "-export"  => spawn(export::export(msg.0, Arc::clone(&state))),
-                _category  => {}
-              }
-            } else if let Some(bug_number) = contains_bug(msg.content.as_str()) {
-              spawn(bug(msg.0, Some(bug_number), Arc::clone(&state)))
-            }
-          },
-          None              => {}
-        }
-      }
-    }
+    Event::MessageCreate(msg) => handle_message(msg, &state).await,
     Event::Ready(_) => {
-      tracing::info!("Shard is ready")
+      tracing::info!("Shard is ready");
+      Ok(())
     }
-    _ => {}
+    _ => Ok(())
   }
-
-  Ok(())
 }
